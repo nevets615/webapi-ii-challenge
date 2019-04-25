11cee3792717a1a72238010c548dc1afcc79b427
nevets615 <surj316@yahoo.com>
2019-04-25 16:17:54 -0400

    still trying to deploy

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require('./server.js');
 
-
-server.listen(4000, () => {
+const port = process.env.PORT || 4000
+server.listen(port, () => {
   console.log("\n*** API running on port 4k ***\n");
 });
+
